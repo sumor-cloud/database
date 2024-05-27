8bb1a1f17d51f77caa822240587e10e7e7e1032e
dennis <dennis@sumor.com>
2024-05-27 20:07:04 +0800

    dev

--- test/main.test.js
+++ test/main.test.js
@@ -1,8 +1,13 @@
 import { describe, expect, it } from '@jest/globals'
 import database from '../src/index.js'
+import { load } from '@sumor/config'
 
 describe('main', () => {
-  it('init', () => {
+  it('init', async () => {
     expect(database.install).toBeDefined()
+
+    const config = await load(`${process.cwd()}/test/config`, 'DB')
+    expect(config).toBeDefined()
+    expect(config.type).toBeDefined()
   })
 })
